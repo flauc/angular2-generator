1bf4802ce1559f4288702987c5e2bbdfa77e5d79
flauc <filip.lauc93@gmail.com>
2016-03-06 00:23:57 +0100

    Added generation of inital files

--- app.ts
+++ app.ts
@@ -12,45 +12,61 @@ function lower(string) { return string.charAt(0).toLowerCase() + string.slice(1)
 function create(arguments: string[]) {
     let type: string,                                                   // The type of the file to create.. will be equal to arguments[0]
         typeFileLoc: string,                                            // The location of the file to read from
-        name: string,                                                   // Name of the file to create
+        name: string = arguments[arguments.indexOf('-n') + 1],          // Name of the file to create
         newFile: string,                                                // Location of the new file
         fileVars: string[],                                             // Variables to change depending on the arguments
         readData: string,                                               // The data from the read file
         writeData: string = '',                                         // The data to write to the new file after adding variables
-        arrayPos: number = 0;                                           // Keeps track of added variables
+        arrayPos: number = 0,                                           // Keeps track of added variables
+        init: boolean = false,
+
+        // Service specific
+        addToBoot: boolean = false;
 
     switch(arguments[0]) {
+        case '-i':
+        case '-init':
+            init = true;
+            break;
         case '-c':
         case '-component':
             type = 'component';
-            name = arguments[1];
             fileVars = ['geny-' + name.toLowerCase(), capitalize(name) + capitalize(type)];
             break;
         case '-s':
         case '-service':
             type = 'service';
-            name = arguments[1];
             fileVars = [capitalize(name) + capitalize(type)];
+            addToBoot = arguments.indexOf('-b') > -1;
             break;
     }
 
     typeFileLoc = `${genyLoc}/codeFiles/${type}.ts`;
     newFile = location + '/' + lower(name) + '.' + type + '.ts';
 
-    fs.readFile(typeFileLoc, 'utf8', (err, data)=> {
-        if (err) throw err;
-        readData = data;
+    if(init) {
+        fs.createReadStream(`${genyLoc}/codeFiles/init/app.component.ts`)
+            .pipe(fs.createWriteStream(`${location}/app.component.ts`));
+        fs.createReadStream(`${genyLoc}/codeFiles/init/boot.ts`)
+            .pipe(fs.createWriteStream(`${location}/boot.ts`));
+    }
 
-        for(let i = 0; i < readData.length; i++) {
-            if(readData[i] != '~') writeData += readData[i];
-            else {
-                writeData += fileVars[arrayPos];
-                arrayPos++;
+    else {
+        fs.readFile(typeFileLoc, 'utf8', (err, data)=> {
+            if (err) throw err;
+            readData = data;
+
+            for(let i = 0; i < readData.length; i++) {
+                if(readData[i] != '~') writeData += readData[i];
+                else {
+                    writeData += fileVars[arrayPos];
+                    arrayPos++;
+                }
             }
-        }
 
-        fs.writeFile(newFile, writeData, (err)=> { if (err) throw err; });
-    });
+            fs.writeFile(newFile, writeData, (err)=> { if (err) throw err; });
+        });
+    }
 }
 
 create(arguments);
