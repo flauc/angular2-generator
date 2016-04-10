8159f68b6ea543d2a842bd85ca80eb202c74c73c
flauc <filip.lauc93@gmail.com>
2016-04-10 20:20:22 +0200

    chore: Refactored routing.ts

--- sections/create/routing/routing.ts
+++ sections/create/routing/routing.ts
@@ -1,23 +1,23 @@
-const fs = require('fs'),
-    files = require('./files');
+const fs = require("fs"),
+    files = require("./files");
 
 module.exports = {
-    create: function(location: string):void {
+    create: function(location: string): void {
 
         // First round
-        let app = fs.writeFile(`${location}/app.component.ts`, files.app, err=> {if(err) console.error(err)}),
-            index = fs.writeFile(`${location}/index.html`, files.index, err=> {if(err) console.error(err)}),
-            boot =fs.writeFile(`${location}/boot.ts`, files.boot, err=> {if(err) console.error(err)}),
-            tsConfig = fs.writeFile(`${location}/tsconfig.json`, files.tsConfig, err=> {if(err) console.error(err)}),
-            homeDir = fs.mkdir(`${location}/home`, (err)=> {if(err) console.error(err)}),
-            aboutDir = fs.mkdir(`${location}/about`, (err)=> {if(err) console.error(err)});
+        let app = fs.writeFile(`${location}/app.component.ts`, files.app, err => {if (err) console.error(err)}),
+            index = fs.writeFile(`${location}/index.html`, files.index, err => {if (err) console.error(err)}),
+            boot = fs.writeFile(`${location}/boot.ts`, files.boot, err => {if (err) console.error(err)}),
+            tsConfig = fs.writeFile(`${location}/tsconfig.json`, files.tsConfig, err => {if(err) console.error(err)}),
+            homeDir = fs.mkdir(`${location}/home`, (err) => {if (err) console.error(err)}),
+            aboutDir = fs.mkdir(`${location}/about`, (err) => {if (err) console.error(err)});
 
         // Second round
-        let home = fs.writeFile(`${location}/home/home.component.ts`, files.home, err=> {if(err) console.error(err)}),
-            about = fs.writeFile(`${location}/about/about.component.ts`, files.about, err=> {if(err) console.error(err)});
+        let home = fs.writeFile(`${location}/home/home.component.ts`, files.home, err => {if (err) console.error(err)}),
+            about = fs.writeFile(`${location}/about/about.component.ts`, files.about, err => {if (err) console.error(err)});
 
-        Promise.all([app, index, boot, tsConfig, homeDir, aboutDir]).then(()=> {
-            Promise.all([home, about]).then(()=> console.log('Routing app created successfully!'))
+        Promise.all([app, index, boot, tsConfig, homeDir, aboutDir]).then(() => {
+            Promise.all([home, about]).then(() => console.log("Routing app created successfully!"))
         });
     }
 };
