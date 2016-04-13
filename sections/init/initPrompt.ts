eb97bd8e5abac411ae2d3793313092fb09b40cf0
flauc <filip.lauc93@gmail.com>
2016-04-13 19:52:31 +0200

    chore: Edited init prompt text

--- sections/init/initPrompt.ts
+++ sections/init/initPrompt.ts
@@ -1,7 +1,9 @@
 export const initPrompt = {
     intro:
-        `Please answer the following in order to
+        `\n************************************************
+        \nPlease answer the following in order to
         \ncreate a folder structure and install all 
         \nthe necessary libraries for your project.
-        \nFor more information check out the documentation.\n`
+        \nFor more information check out the documentation.
+        \n************************************************`
 };
