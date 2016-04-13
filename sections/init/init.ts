29cfa65b4e066f5ed6dee7a29d4e9162f8753874
flauc <filip.lauc93@gmail.com>
2016-04-13 19:43:15 +0200

    feat: Added more prompts on init

--- sections/init/init.ts
+++ sections/init/init.ts
@@ -12,12 +12,20 @@ export default function init() {
         console.log(initPrompt.intro);
 
         co(function *() {
-            let structurePrompt = yield prompt("structure: (standard) "),
-                bootLocationPrompt = yield prompt("bootLocation: (app/boot.ts) ");
+            let appFolderPrompt = yield prompt("App Folder: (./app) "),
+                bootLocationPrompt = yield prompt("Location of bootstrap file: (boot.ts) "),
+                componentsFolderPrompt = yield prompt("Components Folder: (common/components) "),
+                servicesFolderPrompt = yield prompt("Services Folder: (common/services) "),
+                directivesFolderPrompt = yield prompt("Directives Folder: (common/directives) "),
+                pipesFolderPrompt = yield prompt("Pipes Folder: (common/pipes) ");
 
             return {
-                "structure": structurePrompt ? structurePrompt : "standard",
-                "bootLocation": bootLocationPrompt ? bootLocationPrompt : "app/boot.ts"
+                "appFolder": appFolderPrompt ? appFolderPrompt : "./app",
+                "bootLocation": bootLocationPrompt ? bootLocationPrompt : "boot.ts",
+                "componentsFolder": componentsFolderPrompt ? componentsFolderPrompt : "common/components",
+                "servicesFolder": servicesFolderPrompt ? servicesFolderPrompt : "common/services",
+                "directivesFolder": directivesFolderPrompt ? directivesFolderPrompt : "common/directives",
+                "pipesFolder": pipesFolderPrompt ? pipesFolderPrompt : "common/pipes"
             };
 
         }).then(values => {
--- sections/init/initPrompt.ts
+++ sections/init/initPrompt.ts
@@ -1,6 +1,7 @@
 export const initPrompt = {
     intro:
         `Please answer the following in order to
-        \ncreate a genli.json file for your project.\n
+        \ncreate a folder structure and install all 
+        \nthe necessary libraries for your project.
         \nFor more information check out the documentation.\n`
 };
